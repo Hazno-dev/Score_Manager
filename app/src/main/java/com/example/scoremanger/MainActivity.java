342fd0ace20bd9b4e4b99fa30cb84628d8c96dd5
Hazno21688 <62346467+Hazno-dev@users.noreply.github.com>
2021-11-21 20:30:53 +0000

    new pages

--- app/src/main/java/com/example/scoremanger/MainActivity.java
+++ app/src/main/java/com/example/scoremanger/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.scoremanger;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
 import android.widget.EditText;
 import android.os.Bundle;
@@ -10,13 +14,14 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText mUsernameF;
-    EditText mNameF;
-    EditText mEmailF;
+    public static EditText mUsernameF;
+    public static EditText mNameF;
+    public static EditText mEmailF;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createNotificationChannel();
     }
     public void loginClick(View view) {
         Intent intents = new Intent(this, GameList.class);
@@ -37,5 +42,20 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Fields are empty", Toast.LENGTH_SHORT).show();
         } else if (mEmailF.getText().toString().contains("@") && mEmailF.getText().toString().contains("."))startActivity(intents);
         else Toast.makeText(this, "Email Invalid", Toast.LENGTH_SHORT).show();
+    }
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "test";
+            String description = "test description";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("1", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
--- app/src/main/java/com/example/scoremanger/ScoresForGames.java
+++ app/src/main/java/com/example/scoremanger/ScoresForGames.java
@@ -1,13 +1,26 @@
 package com.example.scoremanger;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,13 +29,33 @@ public class ScoresForGames extends AppCompatActivity {
     ListView listviewer;
     List list = new ArrayList();
     ArrayAdapter adapters;
+    NotificationManager NM;
+    Intent PushIntent;
+    NotificationCompat.Builder builder;
 
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scores_for_games);
 
+        PushIntent = new Intent(this, GameList.class);
+        PushIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, PushIntent, 0);
+
+        builder = new NotificationCompat.Builder(this, "1")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Oh no D:")
+                .setContentText("Someone has overtaken your score!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                // Set the intent that will fire when the user taps the notification
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
         listviewer = (ListView)findViewById(R.id.ScoresList);
+        adapters = new ArrayAdapter(ScoresForGames.this, android.R.layout.simple_list_item_1, list);
+        listviewer.setAdapter(adapters);
         ListAdder("HELLO", "43");
         ListAdder("HELLO", "42352353");
         ListAdder("HELDDDLO", "432323");
@@ -30,18 +63,24 @@ public class ScoresForGames extends AppCompatActivity {
         ListAdder("HESSFLLO", "43333");
         ListAdder("HEDFSGDLLO", "42352353");
         ListAdder("HESDGSGSLLO", "432525253");
-        adapters = new ArrayAdapter(ScoresForGames.this, android.R.layout.simple_list_item_1, list);
-        listviewer.setAdapter(adapters);
-
     }
         EditText mNumberF;
     public void ListAdder(String Username, String Score){
         String complete = Username;
         list.add(complete + " - " + Score);
+        adapters.add(Username + Score);
+        adapters.notifyDataSetChanged();
+
     }
     public void ConfirmScores(View view){
         mNumberF = (EditText)findViewById(R.id.ScoreInput);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+// notificationId is a unique int for each notification that you must define
+        notificationManager.notify(1, builder.build());
+
         if (mNumberF.getText().toString().isEmpty()) Toast.makeText(this, "Fields are empty", Toast.LENGTH_SHORT).show();
+        else if (MainActivity.mUsernameF != null) ListAdder(MainActivity.mUsernameF.getText().toString(), mNumberF.getText().toString());
 
     }
 }
